f64247936dc3fc4717d3701efa6f9cc4c6c79cdf
Opal <guerre5lily@gmail.com>
2021-12-07 22:17:19 -0800

    add getStats and getInventory functions

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,7 +1,8 @@
 const Potion = require('../lib/Potion')
 jest.mock('../lib/Potion')
-console.log(new Potion())
+
 const Player = require('../lib/Player.js')
+
 
 test('creates a player object', () => {
     const player = new Player('Dave')
@@ -14,4 +15,24 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     )
+})
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave')
+
+    expect(player.getStats()).toHaveProperty('potions')
+    expect(player.getStats()).toHaveProperty('health')
+    expect(player.getStats()).toHaveProperty('strength')
+    expect(player.getStats()).toHaveProperty('agility')
+
+})
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave')
+
+    expect(player.getInventory()).toEqual(expect.any(Array))
+
+    player.inventory = []
+
+    expect(player.getInventory()).toEqual(false)
 })
